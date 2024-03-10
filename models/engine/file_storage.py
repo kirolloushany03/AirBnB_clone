14918f3d217c469336dfd38fca4543f8571be476
Ashraf Atef <ashraf.programer@gmail.com>
2024-03-10 23:37:29 +0200

    task

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,104 +1,55 @@
 #!/usr/bin/python3
-
-"""FileStorage module providing file-based storage for instances of classes.
-
-This module defines the FileStorage class, which serves as a link between
-JSON files and the program. It provides methods to save, reload, create,
-and retrieve instances of various classes such as User, State, City, Amenity,
-Place, Review, and BaseModel.
-
-Classes:
-    FileStorage: A class providing file-based storage functionality.
-
-Usage:
-    To use the FileStorage class, import it from this module:
-        from models.engine.file_storage import FileStorage
-
-    Create an instance of the FileStorage class and use its methods to
-    manage instances of classes.
-
-Example:
-    # Create a new FileStorage instance
-    storage = FileStorage()
-
-    # Save objects to the file
-    storage.save()
-
-    # Reload objects from the file
-    storage.reload()
-
-Methods:
-    all: Returns a dictionary containing all stored objects.
-    new: Adds a new object to the storage dictionary.
-    save: Saves the objects to a JSON file.
-    reload: Reloads the objects from a JSON file.
-    get_class: Retrieves the class associated with the class name.
-
-Attributes:
-    __file_path (str): The file path for storing JSON data.
-    __objects (dict): A dictionary containing stored objects.
-
-"""
-
-
+"""file_storage moduel contains the FileStorage class"""
 import json
 
-from models.user import User
-from models.state import State
-from models.city import City
-from models.amenity import Amenity
-from models.place import Place
-from models.review import Review
-from models.base_model import BaseModel
 
-
-class FileStorage:  # link between json and our program
+class FileStorage():
+    """The class FileStorage  that serializes instances to\
+        a JSON file and deserializes JSON file to instances"""
 
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns a dictionary containing all stored objects."""
+        """Return FileStorage.__objects 'all storage objects list'"""
         return FileStorage.__objects
 
     def new(self, obj):
-        """Adds a new object to the storage dictionary."""
+        """Add the new object to the objects list"""
         key = f"{obj.__class__.__name__}.{obj.id}"
         self.all()[key] = obj
 
     def save(self):
-        """Saves the objects to a JSON file."""
-        data = {}
-        for key, value in FileStorage.__objects.items():
-            data[key] = value.to_dict()
-        with open(self.__file_path, "w") as file:
-            json.dump(data, file)
+        """save the objects list as a json file"""
+        json_dict = {}
+        for key, value in self.all().items():
+            json_dict[key] = value.to_dict()
+        with open(self.__file_path, "w") as fp:
+            json.dump(json_dict, fp)
 
     def reload(self):
-        """Reloads the objects from a JSON file."""
+        """Reload the objects list from the json file"""
+
         try:
-            with open(self.__file_path, "r") as file:
-                data = json.load(file)  # convert json to python dictionary
+            with open(self.__file_path, "r") as fp:
+                json_dict = json.load(fp)
         except FileNotFoundError:
             return
-        for key, value in data.items():
-            # class_name, obj_id = key.split(".")
-            # cls = eval(self.class_name)
-            # cls = globals().get(class_name)
-            cls = self.get_class(value["__class__"])
-            if cls:
-                self.__objects[key] = cls(**value)
+        for key, value in json_dict.items():
+            self.all()[key] = self.create_object(value["__class__"])(**value)
 
     @staticmethod
-    def get_class(class_name):
-        """Retrieves the class associated with the class name."""
-        mapping_class = {
-            "BaseModel": BaseModel,
-            "User": User,
-            "State": State,
-            "City": City,
-            "Amenity": Amenity,
-            "Place": Place,
-            "Review": Review,
-        }
-        return mapping_class.get(class_name)
+    def create_object(class_name):
+        """Create an instance from a class name"""
+        from models.base_model import BaseModel
+        from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.amenity import Amenity
+        from models.place import Place
+        from models.review import Review
+
+        class_dict = {"BaseModel": BaseModel, "User": User, "State": State,
+                      "City": City, "Amenity": Amenity, "Place": Place,
+                      "Review": Review}
+        return class_dict[class_name]
--- models/user.py
+++ models/user.py
@@ -1,43 +1,5 @@
 #!/usr/bin/python3
-
-"""User module defining the User class.
-
-This module contains the definition of the User class, which
-inherits attributes and methods from the BaseModel class.
-
-Classes:
-    User: A class representing a user in the HBNB project.
-
-Usage:
-    To use the User class, import it from this module:
-        from models.user import User
-
-    Create instances of the User class and use its methods and
-    attributes as needed.
-
-Example:
-    # Create a new User instance
-    user1 = User(email="user@example.com", password="password")
-
-    # Access attributes of the User instance
-    print(user1.id)          # Output: <user_id>
-    print(user1.email)       # Output: user@example.com
-    print(user1.password)    # Output: password
-    print(user1.first_name)  # Output: ''
-    print(user1.last_name)   # Output: ''
-
-Attributes:
-    email (str): The email address of the user.
-    password (str): The password of the user.
-    first_name (str): The first name of the user.
-    last_name (str): The last name of the user.
-
-Methods:
-    __init__: Initializes a User instance.
-
-"""
-
-
+"""The module contains the class User that inherits from BaseModel"""
 from models.base_model import BaseModel
 
 
